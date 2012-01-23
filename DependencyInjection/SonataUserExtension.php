49b986648abad6c6ca2380162f1ded6c4d76a9d9
Thomas Rabaix <thomas.rabaix@ekino.com>
2012-01-23 16:56:12 +0100

    Fix use statement

--- DependencyInjection/SonataUserExtension.php
+++ DependencyInjection/SonataUserExtension.php
@@ -13,13 +13,10 @@ namespace Sonata\UserBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Config\Definition\Processor;
 
 use Sonata\EasyExtendsBundle\Mapper\DoctrineCollector;
 
