d3f0c02804411a70b4006115f0bcd6c07e857aa1
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2012-12-23 00:58:17 +0100

    Updated `AdminSecurity:loginAction()` signature to be compatible with new FOSUserBundle.

--- Controller/AdminSecurityController.php
+++ Controller/AdminSecurityController.php
@@ -17,16 +17,15 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminSecurityController extends SecurityController
 {
     /**
      * {@inheritdoc}
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        $request = $this->container->get('request');
-        /* @var $request \Symfony\Component\HttpFoundation\Request */
         $session = $request->getSession();
         /* @var $session \Symfony\Component\HttpFoundation\Session */
 
