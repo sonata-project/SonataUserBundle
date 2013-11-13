c3523bda3055d19db007cf43a05d5a36c556103e
Hugo Briand <briand@ekino.com>
2013-11-13 12:13:23 +0100

    Refactored ProfileMenuBlock to inherit BlockBundle's MenuBlock

--- Block/ProfileMenuBlockService.php
+++ Block/ProfileMenuBlockService.php
@@ -11,10 +11,11 @@
 
 namespace Sonata\UserBundle\Block;
 
-use Sonata\AdminBundle\Form\FormMapper;
+use Knp\Menu\ItemInterface;
+use Knp\Menu\Provider\MenuProviderInterface;
 use Sonata\AdminBundle\Validator\ErrorElement;
-use Sonata\BlockBundle\Block\BaseBlockService;
 use Sonata\BlockBundle\Block\BlockContextInterface;
+use Sonata\BlockBundle\Block\Service\MenuBlockService;
 use Sonata\BlockBundle\Model\BlockInterface;
 use Sonata\UserBundle\Menu\ProfileMenuBuilder;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
@@ -28,7 +29,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  *
  * @author Hugo Briand <briand@ekino.com>
  */
-class ProfileMenuBlockService extends BaseBlockService
+class ProfileMenuBlockService extends MenuBlockService
 {
     /**
      * @var ProfileMenuBuilder
@@ -38,74 +39,16 @@ class ProfileMenuBlockService extends BaseBlockService
     /**
      * Constructor
      *
-     * @param string             $name
-     * @param EngineInterface    $templating
-     * @param ProfileMenuBuilder $menuBuilder
+     * @param string                $name
+     * @param EngineInterface       $templating
+     * @param MenuProviderInterface $menuProvider
+     * @param ProfileMenuBuilder    $menuBuilder
      */
-    public function __construct($name, EngineInterface $templating, ProfileMenuBuilder $menuBuilder)
+    public function __construct($name, EngineInterface $templating, MenuProviderInterface $menuProvider, ProfileMenuBuilder $menuBuilder)
     {
-        parent::__construct($name, $templating);
+        parent::__construct($name, $templating, $menuProvider);
 
         $this->menuBuilder = $menuBuilder;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function execute(BlockContextInterface $blockContext, Response $response = null)
-    {
-        if ("" === ($menu = $blockContext->getSetting('menu_name')) || null === $menu) {
-            $menu = $this->menuBuilder->createProfileMenu(array('childrenAttributes' => array('class' => $blockContext->getSetting('menu_class'))));
-            $menu->setCurrentUri($blockContext->getSetting('current_uri'));
-        }
-
-        return $this->renderPrivateResponse($blockContext->getTemplate(), array(
-            'menu'         => $menu,
-            'menu_options' => $this->getMenuOptions($blockContext->getSettings()),
-            'block'        => $blockContext->getBlock(),
-            'context'      => $blockContext
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildEditForm(FormMapper $form, BlockInterface $block)
-    {
-        $form->add('settings', 'sonata_type_immutable_array', array(
-            'keys' => array(
-                array('title', 'text', array('required' => false)),
-                array('menu_name', 'string', array('required' => false)),
-                array('menu_class'), 'string', array('required' => false),
-                array('current_class', 'string', array('required' => false)),
-                array('first_class', 'string', array('required' => false)),
-                array('last_class', 'string', array('required' => false)),
-            )
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validateBlock(ErrorElement $errorElement, BlockInterface $block)
-    {
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setDefaultSettings(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'title'         => 'User Profile Menu',
-            'template'      => 'SonataUserBundle:Block:profile_menu.html.twig',
-            'menu_name'     => "",
-            'menu_class'    => "nav nav-list",
-            'current_class' => 'active',
-            'first_class'   => false,
-            'last_class'    => false,
-            'current_uri'   => null
-        ));
     }
 
     /**
@@ -117,26 +60,17 @@ class ProfileMenuBlockService extends BaseBlockService
     }
 
     /**
-     * Replaces setting keys with knp menu item options keys
-     *
-     * @param array $settings
+     * {@inheritdoc}
      */
-    protected function getMenuOptions(array $settings)
+    protected function getMenu(array $settings)
     {
-        $mapping = array(
-            'current_class' => 'currentClass',
-            'first_class'   => 'firstClass',
-            'last_class'    => 'lastClass'
-        );
+        $menu = parent::getMenu($settings);
 
-        $options = array();
-
-        foreach ($settings as $key => $value) {
-            if (array_key_exists($key, $mapping)) {
-                $options[$mapping[$key]] = $value;
-            }
+        if (null === $menu || "" === $menu) {
+            $menu = $this->menuBuilder->createProfileMenu(array('childrenAttributes' => array('class' => $settings['menu_class'])));
+            $menu->setCurrentUri($settings['current_uri']);
         }
 
-        return $options;
+        return $menu;
     }
 }
