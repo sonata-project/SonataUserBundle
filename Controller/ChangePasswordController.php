76d0b8ba395a11bf6e600ba89bbdedfb5a3055e7
Hugo Briand <briand@ekino.com>
2014-01-17 14:41:57 +0100

    Redesigned profile edition page (implemented bootstrap3 as well)

--- Controller/ChangePasswordController.php
+++ Controller/ChangePasswordController.php
@@ -12,6 +12,9 @@ namespace Sonata\UserBundle\Controller;
 
 use FOS\UserBundle\Controller\ChangePasswordController as BaseController;
 use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Class ChangePasswordController
@@ -22,6 +25,29 @@ use FOS\UserBundle\Model\UserInterface;
  */
 class ChangePasswordController extends BaseController
 {
+    public function changePasswordAction(Request $request)
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $form = $this->container->get('fos_user.change_password.form');
+        $formHandler = $this->container->get('fos_user.change_password.form.handler');
+
+        $process = $formHandler->process($user);
+        if ($process) {
+            $this->setFlash('fos_user_success', 'change_password.flash.success');
+
+            return new RedirectResponse($this->getRedirectionUrl($user));
+        }
+
+        return $this->container->get('templating')->renderResponse(
+            'SonataUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
+            array('form' => $form->createView())
+        );
+    }
+
     /**
      * {@inheritdoc}
      */
