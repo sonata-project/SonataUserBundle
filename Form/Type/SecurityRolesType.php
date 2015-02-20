938c8a56e23afd3482aebf7cd2c86a971f2cffe7
Michał Kurzeja <mkurzeja@accesto.pl>
2015-02-20 08:44:24 +0100

    Fixed #499 - hidden roles are not kept after edit, added new tests to cover all use cases

--- Form/Type/SecurityRolesType.php
+++ Form/Type/SecurityRolesType.php
@@ -58,7 +58,7 @@ class SecurityRolesType extends AbstractType
 
         // POST METHOD
         $formBuilder->addEventListener(FormEvents::PRE_BIND, function(FormEvent $event) use ($transformer) {
-            $transformer->setOriginalRoles($event->getData());
+            $transformer->setOriginalRoles($event->getForm()->getData());
         });
 
         $formBuilder->addModelTransformer($transformer);
