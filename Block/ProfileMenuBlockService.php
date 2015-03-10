ccb2718ca5352fc8ed73efd24bf466151148b3c6
Thomas Rabaix <thomas.rabaix@gmail.com>
2015-03-10 11:35:58 +0100

    migrate ErrorElement

--- Block/ProfileMenuBlockService.php
+++ Block/ProfileMenuBlockService.php
@@ -13,7 +13,7 @@ namespace Sonata\UserBundle\Block;
 
 use Knp\Menu\ItemInterface;
 use Knp\Menu\Provider\MenuProviderInterface;
-use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\CoreBundle\Validator\ErrorElement;
 use Sonata\BlockBundle\Block\BlockContextInterface;
 use Sonata\BlockBundle\Block\Service\MenuBlockService;
 use Sonata\BlockBundle\Model\BlockInterface;
