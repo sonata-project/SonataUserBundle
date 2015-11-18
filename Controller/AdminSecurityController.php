6e32946abb5f6693ea3b18fb5d7912889f8911c4
core23 <mail@core23.de>
2015-11-18 15:43:49 +0100

    Missing import

--- Controller/AdminSecurityController.php
+++ Controller/AdminSecurityController.php
@@ -14,6 +14,7 @@ namespace Sonata\UserBundle\Controller;
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Symfony\Component\Security\Core\SecurityContext;
