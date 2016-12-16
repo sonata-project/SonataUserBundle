d927664c0de239c9ffbba550a8b19bcff6cf3d36
Hans Mackowiak <hanmac@gmx.de>
2016-12-16 15:44:35 +0100

    Update SonataUserExtension.php
    fixed add missing use for Reference

--- DependencyInjection/SonataUserExtension.php
+++ DependencyInjection/SonataUserExtension.php
@@ -16,6 +16,7 @@ use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
