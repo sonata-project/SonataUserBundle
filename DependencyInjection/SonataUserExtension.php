0f41c26ac1a33927c3d39ed1728ca99efe5d696d
Kev391 <rembert.kevin17@gmail.com>
2016-12-16 15:47:19 +0100

    Import Reference class

--- DependencyInjection/SonataUserExtension.php
+++ DependencyInjection/SonataUserExtension.php
@@ -17,6 +17,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * @author     Thomas Rabaix <thomas.rabaix@sonata-project.org>
