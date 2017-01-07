sonata-project/SonataUserBundle

commit 6f37cdf9dc57c0c71ba3b01a1856dfd6f9c686fc

add compatibility for SecurityContextInterface split or not (#812)
SecurityContextInterface was split into two interfaces.
This fix makes our code compatible with both versions.

DependencyInjection/SonataUserExtension.php
@@ -73,6 +73,32 @@ class SonataUserExtension extends Extension
 
         $config = $this->addDefaults($config);
 
+        // Set the SecurityContext for Symfony <2.6
+        // NEXT_MAJOR: Go back to simple xml configuration when bumping requirements to SF 2.6+
+        if (interface_exists('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface')) {
+            $tokenStorageReference = new Reference('security.token_storage');
+            $authorizationCheckerReference = new Reference('security.authorization_checker');
+        } else {
+            $tokenStorageReference = new Reference('security.context');
+            $authorizationCheckerReference = new Reference('security.context');
+        }
+
+        $container
+            ->getDefinition('sonata.user.editable_role_builder')
+            ->replaceArgument(0, $tokenStorageReference)
+            ->replaceArgument(1, $authorizationCheckerReference)
+        ;
+
+        $container
+            ->getDefinition('sonata.user.block.account')
+            ->replaceArgument(2, $tokenStorageReference)
+        ;
+
+        $container
+            ->getDefinition('sonata.user.google.authenticator.request_listener')
+            ->replaceArgument(2, $tokenStorageReference)
+        ;
+
         $this->registerDoctrineMapping($config);
         $this->configureAdminClass($config, $container);
         $this->configureClass($config, $container);

Block/AccountBlockService.php
@@ -13,11 +13,11 @@ namespace Sonata\UserBundle\Block;
 
 use Sonata\BlockBundle\Block\BaseBlockService;
 use Sonata\BlockBundle\Block\BlockContextInterface;
-use Sonata\UserBundle\Menu\ProfileMenuBuilder;
 use Sonata\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
@@ -31,22 +31,30 @@ use Symfony\Component\Security\Core\SecurityContextInterface;
 class AccountBlockService extends BaseBlockService
 {
     /**
-     * @var ProfileMenuBuilder
+     * @var TokenStorageInterface|SecurityContextInterface
      */
-    private $securityContext;
+    private $tokenStorage;
 
     /**
      * Constructor.
      *
-     * @param string                   $name
-     * @param EngineInterface          $templating
-     * @param SecurityContextInterface $securityContext
+     * NEXT_MAJOR: Go back to type hinting check when bumping requirements to SF 2.6+.
+     *
+     * @param string                                         $name
+     * @param EngineInterface                                $templating
+     * @param TokenStorageInterface|SecurityContextInterface $tokenStorage
      */
-    public function __construct($name, EngineInterface $templating, SecurityContextInterface $securityContext)
+    public function __construct($name, EngineInterface $templating, $tokenStorage)
     {
         parent::__construct($name, $templating);
 
-        $this->securityContext = $securityContext;
+        if (!$tokenStorage instanceof TokenStorageInterface && !$tokenStorage instanceof SecurityContextInterface) {
+            throw new \InvalidArgumentException(
+                'Argument 3 should be an instance of Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface or Symfony\Component\Security\Core\SecurityContextInterface'
+            );
+        }
+
+        $this->tokenStorage = $tokenStorage;
     }
 
     /**
@@ -55,8 +63,8 @@ class AccountBlockService extends BaseBlockService
     public function execute(BlockContextInterface $blockContext, Response $response = null)
     {
         $user = false;
-        if ($this->securityContext->getToken()) {
-            $user = $this->securityContext->getToken()->getUser();
+        if ($this->tokenStorage->getToken()) {
+            $user = $this->tokenStorage->getToken()->getUser();
         }
 
         if (!$user instanceof UserInterface) {

Controller/AdminSecurityController.php
@@ -43,7 +43,7 @@ class AdminSecurityController extends Controller
 
         $session = $request->getSession();
 
-        // Symfony <2.6 BC. To be removed.
+        // NEXT_MAJOR: Symfony <2.6 BC. To be removed.
         $authenticationErrorKey = class_exists('Symfony\Component\Security\Core\Security')
             ? Security::AUTHENTICATION_ERROR : SecurityContextInterface::AUTHENTICATION_ERROR;
 
@@ -61,6 +61,7 @@ class AdminSecurityController extends Controller
             $error = null; // The value does not come from the security component.
         }
 
+        // NEXT_MAJOR: Symfony <2.6 BC. To be removed.
         $lastUsernameKey = class_exists('Symfony\Component\Security\Core\Security')
             ? Security::LAST_USERNAME : SecurityContextInterface::LAST_USERNAME;
 

Controller/SecurityFOSUser1Controller.php
@@ -15,6 +15,7 @@ use FOS\UserBundle\Controller\SecurityController;
 use Sonata\UserBundle\Model\UserInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
@@ -51,12 +52,16 @@ class SecurityFOSUser1Controller extends SecurityController
         /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
         $session = $request->getSession();
 
+        // NEXT_MAJOR: Symfony <2.6 BC. To be removed.
+        $authenticationErrorKey = class_exists('Symfony\Component\Security\Core\Security')
+            ? Security::AUTHENTICATION_ERROR : SecurityContextInterface::AUTHENTICATION_ERROR;
+
         // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContextInterface::AUTHENTICATION_ERROR);
-        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
-            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        if ($request->attributes->has(authenticationErrorKey)) {
+            $error = $request->attributes->get(authenticationErrorKey);
+        } elseif (null !== $session && $session->has(authenticationErrorKey)) {
+            $error = $session->get(authenticationErrorKey);
+            $session->remove(authenticationErrorKey);
         } else {
             $error = null;
         }
@@ -65,8 +70,12 @@ class SecurityFOSUser1Controller extends SecurityController
             $error = null; // The value does not come from the security component.
         }
 
+        // NEXT_MAJOR: Symfony <2.6 BC. To be removed.
+        $lastUserNameKey = class_exists('Symfony\Component\Security\Core\Security')
+            ? Security::LAST_USERNAME : SecurityContextInterface::LAST_USERNAME;
+
         return $this->renderLogin(array(
-            'last_username' => (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME),
+            'last_username' => (null === $session) ? '' : $session->get($lastUserNameKey),
             'error' => $error,
             'csrf_token' => $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate'),
         ));

GoogleAuthenticator/RequestListener.php
@@ -14,6 +14,7 @@ namespace Sonata\UserBundle\GoogleAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
@@ -25,9 +26,9 @@ class RequestListener
     protected $helper;
 
     /**
-     * @var SecurityContextInterface
+     * @var TokenStorageInterface|SecurityContextInterface
      */
-    protected $securityContext;
+    protected $tokenStorage;
 
     /**
      * @var EngineInterface
@@ -35,14 +36,22 @@ class RequestListener
     protected $templating;
 
     /**
-     * @param Helper                   $helper
-     * @param SecurityContextInterface $securityContext
-     * @param EngineInterface          $templating
+     * NEXT_MAJOR: Go back to type hinting check when bumping requirements to SF 2.6+.
+     *
+     * @param Helper                                         $helper
+     * @param TokenStorageInterface|SecurityContextInterface $tokenStorage
+     * @param EngineInterface                                $templating
      */
-    public function __construct(Helper $helper, SecurityContextInterface $securityContext, EngineInterface $templating)
+    public function __construct(Helper $helper, $tokenStorage, EngineInterface $templating)
     {
+        if (!$tokenStorage instanceof TokenStorageInterface && !$tokenStorage instanceof SecurityContextInterface) {
+            throw new \InvalidArgumentException(
+                'Argument 2 should be an instance of Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface or Symfony\Component\Security\Core\SecurityContextInterface'
+            );
+        }
+
         $this->helper = $helper;
-        $this->securityContext = $securityContext;
+        $this->tokenStorage = $tokenStorage;
         $this->templating = $templating;
     }
 
@@ -55,7 +64,7 @@ class RequestListener
             return;
         }
 
-        $token = $this->securityContext->getToken();
+        $token = $this->tokenStorage->getToken();
 
         if (!$token) {
             return;
@@ -65,10 +74,10 @@ class RequestListener
             return;
         }
 
-        $key = $this->helper->getSessionKey($this->securityContext->getToken());
+        $key = $this->helper->getSessionKey($token);
         $request = $event->getRequest();
         $session = $event->getRequest()->getSession();
-        $user = $this->securityContext->getToken()->getUser();
+        $user = $token->getUser();
 
         if (!$session->has($key)) {
             return;

Resources/config/admin.xml
@@ -2,7 +2,8 @@
 <container xmlns="http://symfony.com/schema/dic/services" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://symfony.com/schema/dic/services http://symfony.com/schema/dic/services/services-1.0.xsd">
     <services>
         <service id="sonata.user.editable_role_builder" class="Sonata\UserBundle\Security\EditableRolesBuilder">
-            <argument type="service" id="security.context"/>
+            <argument/>
+            <argument/>
             <argument type="service" id="sonata.admin.pool"/>
             <argument>%security.role_hierarchy.roles%</argument>
         </service>

Resources/config/block.xml
@@ -12,7 +12,7 @@
             <tag name="sonata.block"/>
             <argument>sonata.user.block.account</argument>
             <argument type="service" id="templating"/>
-            <argument type="service" id="security.context"/>
+            <argument/>
         </service>
     </services>
 </container>

Resources/config/google_authenticator.xml
@@ -15,7 +15,7 @@
         <service id="sonata.user.google.authenticator.request_listener" class="Sonata\UserBundle\GoogleAuthenticator\RequestListener">
             <tag name="kernel.event_listener" event="kernel.request" method="onCoreRequest" priority="-1"/>
             <argument type="service" id="sonata.user.google.authenticator.provider"/>
-            <argument type="service" id="security.context"/>
+            <argument/>
             <argument type="service" id="templating"/>
         </service>
     </services>

commit 32d9c7f11e1f151b4b2d9ca776ec6a7e1020d41a

fixed argument replacing position
This bug was introduced in https://github.com/sonata-project/SonataUserBundle/pull/812. The argument index starts at 0, not at 1.

DependencyInjection/SonataUserExtension.php
@@ -99,7 +99,7 @@ class SonataUserExtension extends Extension
 
         $container
             ->getDefinition('sonata.user.google.authenticator.request_listener')
-            ->replaceArgument(2, $tokenStorageReference)
+            ->replaceArgument(1, $tokenStorageReference)
         ;
 
         $this->registerDoctrineMapping($config);