94de49a732609346c8f956b2e601c2dae921c811
Grégoire Paris <postmaster@greg0ire.fr>
2020-01-30 19:52:56 +0100

    Upgrade DI test lib

--- tests/DependencyInjection/SonataUserExtensionTest.php
+++ tests/DependencyInjection/SonataUserExtensionTest.php
@@ -276,7 +276,7 @@ final class SonataUserExtensionTest extends AbstractExtensionTestCase
     /**
      * {@inheritdoc}
      */
-    protected function getMinimalConfiguration()
+    protected function getMinimalConfiguration(): array
     {
         return (new Processor())->process((new Configuration())->getConfigTreeBuilder()->buildTree(), []);
     }
@@ -289,21 +289,5 @@ final class SonataUserExtensionTest extends AbstractExtensionTestCase
         return [
             new SonataUserExtension(),
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function load(array $configurationValues = []): void
-    {
-        $configs = [$this->getMinimalConfiguration(), $configurationValues];
-
-        foreach ($this->container->getExtensions() as $extension) {
-            if ($extension instanceof PrependExtensionInterface) {
-                $extension->prepend($this->container);
-            }
-
-            $extension->load($configs, $this->container);
-        }
     }
 }
