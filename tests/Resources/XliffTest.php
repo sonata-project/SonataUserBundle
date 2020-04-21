1664aca257996c067093ad94e23c01ec101b01d7
Sullivan SENECHAL <soullivaneuh@gmail.com>
2020-04-21 04:06:13 +0200

    Applied fixes from FlintCI

--- tests/Resources/XliffTest.php
+++ tests/Resources/XliffTest.php
@@ -29,7 +29,7 @@ class XliffTest extends TestCase
      */
     private $errors = [];
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         $this->loader = new XliffFileLoader();
     }
