3fefcb6a679157c7fa112ee38a40fadb28036dfa
Wojciech Błoszyk <wbloszyk@gmail.com>
2020-10-12 20:19:28 +0200

    Some fixes

--- src/Util/PasswordUpdater.php
+++ src/Util/PasswordUpdater.php
@@ -33,7 +33,7 @@ class PasswordUpdater implements PasswordUpdaterInterface
 
     public function hashPassword(UserInterface $user)
     {
-        $plainPassword = $user->getPlainPassword();
+        $plainPassword = $user->getPlainPassword() ?? '';
 
         if (0 === \strlen($plainPassword)) {
             return;
