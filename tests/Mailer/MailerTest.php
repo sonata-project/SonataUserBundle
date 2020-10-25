ecc961b15218627ee1106f2f53679fcc9ef27efd
Javier Spagnoletti <phansys@gmail.com>
2020-10-25 13:19:05 -0300

    Allow to use "symfony/mailer" within `Mailer`

--- tests/Mailer/MailerTest.php
+++ tests/Mailer/MailerTest.php
@@ -17,6 +17,8 @@ use FOS\UserBundle\Model\UserInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Sonata\UserBundle\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface as SymfonyMailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\RouterInterface;
 use Twig\Environment;
 
@@ -33,7 +35,7 @@ class MailerTest extends TestCase
     private $templating;
 
     /**
-     * @var \Swift_Mailer|MockObject
+     * @var SymfonyMailerInterface|MockObject
      */
     private $mailer;
 
@@ -51,8 +53,10 @@ class MailerTest extends TestCase
     {
         $this->router = $this->createMock(RouterInterface::class);
         $this->templating = $this->createMock(Environment::class);
-        $this->mailer = $this->createMock(\Swift_Mailer::class);
-        $this->emailFrom = ['noreply@sonata-project.org'];
+        $this->mailer = $this->createMock(SymfonyMailerInterface::class);
+        $this->emailFrom = [
+            'noreply@sonata-project.org' => 'Sonata Project',
+        ];
         $this->template = 'foo';
     }
 
@@ -71,7 +75,7 @@ class MailerTest extends TestCase
      */
     public function testSendResettingEmailMessage(string $template, string $subject, string $body): void
     {
-        $user = $this->createMock(UserInterface::class);
+        $user = $this->createStub(UserInterface::class);
         $user
             ->method('getConfirmationToken')
             ->willReturn('user-token');
@@ -89,16 +93,61 @@ class MailerTest extends TestCase
             ->with('foo', ['user' => $user, 'confirmationUrl' => '/foo'])
             ->willReturn($template);
 
+        $fromName = current($this->emailFrom);
+        $fromAddress = current(array_keys($this->emailFrom));
+
+        $email = (new Email())
+            ->from(sprintf('%s <%s>', $fromName, $fromAddress))
+            ->to((string) $user->getEmail())
+            ->subject($subject)
+            ->html($body);
+
         $this->mailer->expects($this->once())
             ->method('send')
-            ->willReturnCallback(function (\Swift_Message $message) use ($subject, $body): void {
-                $this->assertSame($subject, $message->getSubject());
-                $this->assertSame($body, $message->getBody());
-                $this->assertArrayHasKey($this->emailFrom[0], $message->getFrom());
-                $this->assertArrayHasKey('user@sonata-project.org', $message->getTo());
-            });
+            ->with($this->equalTo($email));
 
         $this->getMailer()->sendResettingEmailMessage($user);
+    }
+
+    /**
+     * NEXT_MAJOR: Remove this method.
+     *
+     * @group legacy
+     *
+     * @dataProvider emailTemplateData
+     */
+    public function testSendResettingEmailMessageWithSwiftMailer(string $template, string $subject, string $body): void
+    {
+        $user = $this->createStub(UserInterface::class);
+        $user
+            ->method('getConfirmationToken')
+            ->willReturn('user-token');
+        $user
+            ->method('getEmail')
+            ->willReturn('user@sonata-project.org');
+
+        $this->router->expects($this->once())
+            ->method('generate')
+            ->with('sonata_user_admin_resetting_reset', ['token' => 'user-token'])
+            ->willReturn('/foo');
+
+        $this->templating->expects($this->once())
+            ->method('render')
+            ->with('foo', ['user' => $user, 'confirmationUrl' => '/foo'])
+            ->willReturn($template);
+
+        $swiftMailer = $this->createMock(\Swift_Mailer::class);
+
+        $swiftMailer->expects($this->once())
+            ->method('send')
+            ->with($this->callback(function (\Swift_Message $message) use ($subject, $body, $user): bool {
+                return $subject === $message->getSubject()
+                    && $body === $message->getBody()
+                    && $this->emailFrom === $message->getFrom()
+                    && \array_key_exists((string) $user->getEmail(), $message->getTo());
+            }));
+
+        (new Mailer($this->router, $this->templating, $swiftMailer, $this->emailFrom, $this->template))->sendResettingEmailMessage($user);
     }
 
     public function emailTemplateData(): array
