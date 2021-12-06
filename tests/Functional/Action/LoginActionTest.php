9265df80673f2dc31a194568c12de23aecf6d4ee
Jordi Sala Morales <jordism91@gmail.com>
2021-12-06 11:25:43 +0100

    Keep adding test for Request password

--- tests/Functional/Action/LoginActionTest.php
+++ tests/Functional/Action/LoginActionTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sonata\UserBundle\Tests\Functional;
+namespace Sonata\UserBundle\Tests\Functional\Action;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Sonata\UserBundle\Tests\App\AppKernel;
