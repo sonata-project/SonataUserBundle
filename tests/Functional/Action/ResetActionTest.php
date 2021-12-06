2b5e5c26f023a7d115bc3e655cbd97677cc7efab
Jordi Sala Morales <jordism91@gmail.com>
2021-12-06 12:12:56 +0100

    Add missing reset password tests

---
+++ tests/Functional/Action/ResetActionTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\UserBundle\Tests\Functional\Action;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sonata\UserBundle\Tests\App\AppKernel;
+use Sonata\UserBundle\Tests\App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ResetActionTest extends WebTestCase
+{
+    public function testItRedirectsToResetPasswordRequestOnInvalidToken(): void
+    {
+        $client = static::createClient();
+        $client->catchExceptions(true);
+        $client->request('GET', '/reset/25');
+
+        static::assertResponseStatusCodeSame(404);
+    }
+
+    public function testItResetsPassword(): void
+    {
+        $client = static::createClient();
+
+        $user = $this->prepareData();
+
+        static::assertSame($user->getPassword(), 'random_password');
+
+        $client->request('GET', sprintf('/reset/%s', $user->getConfirmationToken()));
+
+        $client->submitForm('submit', [
+            'fos_user_resetting_form[plainPassword][first]' => 'new_password',
+            'fos_user_resetting_form[plainPassword][second]' => 'new_password',
+        ]);
+        $client->followRedirect();
+
+        static::assertRouteSame('sonata_admin_dashboard');
+
+        $user = $this->refreshUser($user);
+
+        static::assertNull($user->getPasswordRequestedAt());
+        static::assertNull($user->getConfirmationToken());
+        static::assertSame($user->getPassword(), sprintf('new_password{%s}', $user->getSalt()));
+    }
+
+    /**
+     * @return class-string<\Symfony\Component\HttpKernel\KernelInterface>
+     */
+    protected static function getKernelClass(): string
+    {
+        return AppKernel::class;
+    }
+
+    private function prepareData(): User
+    {
+        $manager = self::$container->get('doctrine.orm.entity_manager');
+        \assert($manager instanceof EntityManagerInterface);
+
+        $user = new User();
+        $user->setUsername('username');
+        $user->setEmail('email@localhost.com');
+        $user->setEmailCanonical('email@localhost.com');
+        $user->setPassword('random_password');
+        $user->setConfirmationToken('confirmation_token');
+        $user->setPasswordRequestedAt(new \DateTime());
+        $user->setSuperAdmin(true);
+        $user->setEnabled(true);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        return $user;
+    }
+
+    private function refreshUser(User $user): User
+    {
+        $manager = self::$container->get('doctrine.orm.entity_manager');
+        \assert($manager instanceof EntityManagerInterface);
+
+        return $manager->find(User::class, $user->getId());
+    }
+}
