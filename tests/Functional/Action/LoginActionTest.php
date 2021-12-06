6e344e5630a3825fa3d718d3afda602193dcf6f2
Jordi Sala Morales <jordism91@gmail.com>
2021-12-06 11:45:42 +0100

    More reset password tests

--- tests/Functional/Action/LoginActionTest.php
+++ tests/Functional/Action/LoginActionTest.php
@@ -56,7 +56,7 @@ class LoginActionTest extends WebTestCase
         static::assertResponseIsSuccessful();
 
         $client->submitForm('submit', [
-            '_username' => 'email@localhost',
+            '_username' => 'email@localhost.com',
             '_password' => 'random_password',
         ]);
         $client->followRedirect();
@@ -79,8 +79,8 @@ class LoginActionTest extends WebTestCase
 
         $user = new User();
         $user->setUsername('username');
-        $user->setEmail('email@localhost');
-        $user->setEmailCanonical('email@localhost');
+        $user->setEmail('email@localhost.com');
+        $user->setEmailCanonical('email@localhost.com');
         $user->setPassword('random_password');
         $user->setSuperAdmin(true);
         $user->setEnabled($enabled);
