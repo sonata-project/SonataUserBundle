c6139512bbaa38d0bc32844ffa08cd41e8d65327
Jordi Sala Morales <jordism91@gmail.com>
2022-01-09 17:41:50 +0100

    Fix tests

--- tests/Security/EditableRolesBuilderTest.php
+++ tests/Security/EditableRolesBuilderTest.php
@@ -13,23 +13,51 @@ declare(strict_types=1);
 
 namespace Sonata\UserBundle\Tests\Security\Authorization\Voter;
 
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\MockObject\Stub;
 use PHPUnit\Framework\TestCase;
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Admin\Pool;
 use Sonata\AdminBundle\Security\Handler\SecurityHandlerInterface;
 use Sonata\AdminBundle\SonataConfiguration;
 use Sonata\UserBundle\Security\EditableRolesBuilder;
+use Sonata\UserBundle\Security\EditableRolesBuilderInterface;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class EditableRolesBuilderTest extends TestCase
 {
+    /**
+     * @var MockObject&TokenStorageInterface
+     */
+    private MockObject $tokenStorage;
+
+    /**
+     * @var MockObject&AuthorizationCheckerInterface
+     */
+    private MockObject $authorizationChecker;
+
+    private Container $container;
+
+    private Pool $pool;
+
     private SonataConfiguration $configuration;
+
+    /**
+     * @var Stub&TranslatorInterface
+     */
+    private Stub $translator;
 
     protected function setUp(): void
     {
+        $this->tokenStorage = $this->createMock(TokenStorageInterface::class);
+        $this->authorizationChecker = $this->createMock(AuthorizationCheckerInterface::class);
+
+        $this->container = new Container();
+        $this->pool = new Pool($this->container);
         $this->configuration = new SonataConfiguration('title', 'logo', [
             'confirm_exit' => true,
             'default_group' => 'group',
@@ -56,24 +84,18 @@ class EditableRolesBuilderTest extends TestCase
             'use_select2' => true,
             'use_stickyforms' => true,
         ]);
+
+        $this->translator = $this->createStub(TranslatorInterface::class);
     }
 
-    /**
-     * @group legacy
-     */
     public function testRolesFromHierarchy(): void
     {
-        $token = $this->createMock(TokenInterface::class);
+        $this->tokenStorage->method('getToken')->willReturn(
+            $this->createStub(TokenInterface::class)
+        );
+        $this->authorizationChecker->method('isGranted')->willReturn(true);
 
-        $tokenStorage = $this->createMock(TokenStorageInterface::class);
-        $tokenStorage->method('getToken')->willReturn($token);
-
-        $authorizationChecker = $this->createMock(AuthorizationCheckerInterface::class);
-        $authorizationChecker->method('isGranted')->willReturn(true);
-
-        $pool = new Pool(new Container());
-
-        $rolesHierarchy = [
+        $builder = $this->getEditableRolesBuilder([
             'ROLE_ADMIN' => [
                 0 => 'ROLE_USER',
             ],
@@ -86,9 +108,13 @@ class EditableRolesBuilderTest extends TestCase
                 5 => 'ROLE_SONATA_PAGE_ADMIN_BLOCK_EDIT',
             ],
             'SONATA' => [],
-        ];
+        ]);
 
-        $expected = [
+        $roles = $builder->getRoles();
+        $rolesReadOnly = $builder->getRolesReadOnly();
+
+        static::assertEmpty($rolesReadOnly);
+        static::assertSame([
             'ROLE_ADMIN' => 'ROLE_ADMIN: ROLE_USER',
             'ROLE_USER' => 'ROLE_USER',
             'ROLE_SUPER_ADMIN' => 'ROLE_SUPER_ADMIN: ROLE_USER, ROLE_SONATA_ADMIN, ROLE_ADMIN, ROLE_ALLOWED_TO_SWITCH, ROLE_SONATA_PAGE_ADMIN_PAGE_EDIT, ROLE_SONATA_PAGE_ADMIN_BLOCK_EDIT',
@@ -97,14 +123,7 @@ class EditableRolesBuilderTest extends TestCase
             'ROLE_SONATA_PAGE_ADMIN_PAGE_EDIT' => 'ROLE_SONATA_PAGE_ADMIN_PAGE_EDIT',
             'ROLE_SONATA_PAGE_ADMIN_BLOCK_EDIT' => 'ROLE_SONATA_PAGE_ADMIN_BLOCK_EDIT',
             'SONATA' => 'SONATA: ',
-        ];
-
-        $builder = new EditableRolesBuilder($tokenStorage, $authorizationChecker, $pool, $this->configuration, null, $rolesHierarchy);
-        $roles = $builder->getRoles();
-        $rolesReadOnly = $builder->getRolesReadOnly();
-
-        static::assertEmpty($rolesReadOnly);
-        static::assertSame($expected, $roles);
+        ], $roles);
     }
 
     public function testRolesFromAdminWithMasterAdmin(): void
@@ -117,50 +136,53 @@ class EditableRolesBuilderTest extends TestCase
         $admin->expects(static::exactly(2))->method('getSecurityInformation')->willReturn(['GUEST' => [0 => 'VIEW', 1 => 'LIST'], 'STAFF' => [0 => 'EDIT', 1 => 'LIST', 2 => 'CREATE'], 'EDITOR' => [0 => 'OPERATOR', 1 => 'EXPORT'], 'ADMIN' => [0 => 'MASTER']]);
         $admin->expects(static::exactly(2))->method('getSecurityHandler')->willReturn($securityHandler);
 
-        $token = $this->createMock(TokenInterface::class);
+        $this->tokenStorage->method('getToken')->willReturn(
+            $this->createStub(TokenInterface::class)
+        );
+        $this->authorizationChecker->method('isGranted')->willReturn(true);
+        $this->container->set('myadmin', $admin);
 
-        $tokenStorage = $this->createMock(TokenStorageInterface::class);
-        $tokenStorage->method('getToken')->willReturn($token);
+        $pool = new Pool($this->container, ['myadmin']);
+        $builder = $this->getEditableRolesBuilder([], $pool);
 
-        $authorizationChecker = $this->createMock(AuthorizationCheckerInterface::class);
-        $authorizationChecker->method('isGranted')->willReturn(true);
+        $roles = $builder->getRoles();
+        $rolesReadOnly = $builder->getRolesReadOnly();
 
-        $container = new Container();
-        $container->set('myadmin', $admin);
-
-        $pool = new Pool($container, ['myadmin']);
-
-        $builder = new EditableRolesBuilder($tokenStorage, $authorizationChecker, $pool, $this->configuration);
-
-        $expected = [
+        static::assertEmpty($rolesReadOnly);
+        static::assertSame([
             'ROLE_FOO_GUEST' => 'ROLE_FOO_GUEST',
             'ROLE_FOO_STAFF' => 'ROLE_FOO_STAFF',
             'ROLE_FOO_EDITOR' => 'ROLE_FOO_EDITOR',
             'ROLE_FOO_ADMIN' => 'ROLE_FOO_ADMIN',
-        ];
-
-        $roles = $builder->getRoles();
-        $rolesReadOnly = $builder->getRolesReadOnly();
-        static::assertEmpty($rolesReadOnly);
-        static::assertSame($expected, $roles);
+        ], $roles);
     }
 
     public function testWithNoSecurityToken(): void
     {
-        $tokenStorage = $this->createMock(TokenStorageInterface::class);
-        $tokenStorage->method('getToken')->willReturn(null);
+        $this->tokenStorage->method('getToken')->willReturn(null);
+        $this->authorizationChecker->method('isGranted')->willReturn(false);
 
-        $authorizationChecker = $this->createMock(AuthorizationCheckerInterface::class);
-        $authorizationChecker->method('isGranted')->willReturn(false);
-
-        $pool = new Pool(new Container());
-
-        $builder = new EditableRolesBuilder($tokenStorage, $authorizationChecker, $pool, $this->configuration);
+        $builder = $this->getEditableRolesBuilder();
 
         $roles = $builder->getRoles();
         $rolesReadOnly = $builder->getRolesReadOnly();
 
         static::assertEmpty($roles);
         static::assertEmpty($rolesReadOnly);
+    }
+
+    /**
+     * @param array<string, array<string>> $rolesHierarchy
+     */
+    private function getEditableRolesBuilder(array $rolesHierarchy = [], ?Pool $pool = null): EditableRolesBuilderInterface
+    {
+        return new EditableRolesBuilder(
+            $this->tokenStorage,
+            $this->authorizationChecker,
+            $pool ?? $this->pool,
+            $this->configuration,
+            $this->translator,
+            $rolesHierarchy
+        );
     }
 }
