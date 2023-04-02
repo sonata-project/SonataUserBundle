f680137a59da28325c9f33c2cfe2d9c8b69ff14c
Jordi Sala Morales <jordism91@gmail.com>
2023-04-02 10:16:50 +0200

    Actualizar UserAdminTest.php

--- tests/Functional/Admin/UserAdminTest.php
+++ tests/Functional/Admin/UserAdminTest.php
@@ -107,7 +107,7 @@ final class UserAdminTest extends WebTestCase
             'uniqid' => 'user',
         ]);
         $client->submitForm('btn_update_and_list', [
-            'user[enabled]' => false
+            'user[enabled]' => false,
             'user[plainPassword]' => 'new_password',
         ]);
         $client->followRedirect();
