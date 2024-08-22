c17f6a446e8f1f01bb9d262cd791a01e6f2c298f
BA-JBI <31063297+BA-JBI@users.noreply.github.com>
2024-08-22 11:03:53 +0200

    Remove usage of username get parameter in CheckEmailAction.php
    Remove usage as discussed in https://github.com/sonata-project/SonataUserBundle/issues/1692#issuecomment-2302032742

--- src/Action/CheckEmailAction.php
+++ src/Action/CheckEmailAction.php
@@ -15,32 +15,21 @@ namespace Sonata\UserBundle\Action;
 
 use Sonata\AdminBundle\Admin\Pool;
 use Sonata\AdminBundle\Templating\TemplateRegistryInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment;
 
 final class CheckEmailAction
 {
     public function __construct(
         private Environment $twig,
-        private UrlGeneratorInterface $urlGenerator,
         private Pool $adminPool,
         private TemplateRegistryInterface $templateRegistry,
         private int $tokenTtl
     ) {
     }
 
-    public function __invoke(Request $request): Response
+    public function __invoke(): Response
     {
-        $username = $request->query->get('username');
-
-        if (null === $username) {
-            // the user does not come from the sendEmail action
-            return new RedirectResponse($this->urlGenerator->generate('sonata_user_admin_resetting_request'));
-        }
-
         return new Response($this->twig->render('@SonataUser/Admin/Security/Resetting/checkEmail.html.twig', [
             'base_template' => $this->templateRegistry->getTemplate('layout'),
             'admin_pool' => $this->adminPool,
